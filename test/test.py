47079778b56398c5bc1e51157874a43f5be2960d
arya1234 <arya.dhorajiya@gmail.com>
2022-08-09 17:16:06 -0400

    Arya Testing Part

--- test/test.py
+++ test/test.py
@@ -7,23 +7,33 @@ def Bag_of_Words_testing(b):
                     [0 ,0 ,0, 0 ,0 ,0 ,1 ,0 ,1 ,1 ,1 ,1]])
     result = np.testing.assert_array_equal(bow_ftransform.BOW_fit_transform(),arr)
     return result
-  
-  
+
+def count_vector_testing(data):
+    bow_vector = Vectorizer()
+    out = CountVectorizer().fit_transform(data).toarray()
+    result = np.testing.assert_array_equal(bow_vector.cv_trans(data),out)
+    return result
 if __name__ == "__main__":  
     
     c = Clean()
-    c = c.remove_punctuation(['She loves pizza, pizza is delicious',
+    x = c.remove_symbol(['She loves pizza&%^$, pizza is ^*&delicious',
+                              'She is a good pers%£%^on',
+                              'good people%..... are%£ the best are'])
+    print('Removed Symbol from input --','\t',x)
+    print('\n')
+    out = c.remove_punctuation(['She loves pizza, pizza is delicious',
                               'She is a good person',
                               'good people..... are the best are'])
-    print('Removed Punctation from input --','\t',x)
-    print('\n')
-    
+    print('Removed Punctation from input --','\t',out)
+    print('\n')   
     ''' below part will be operational only when suddhendras code is complete,
         also input arr will be changed accordingly'''
     # w = Word(c)
     # w = w.join_stopwords()
     # print('Removed stopwords from input --','\t',w) 
-    
+    res_vector = count_vector_testing(out)
+    if result == None:
+        print("Count Vector Method Works, moving forward to Bag of Words")
     result = Bag_of_Words_testing(c)
     if result == None:
         print("Everything is correct")  
